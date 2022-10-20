f0f24c012b9bf1c77b5128d01a9b8080268352e2
ytcaglar <93604446+ytcaglar@users.noreply.github.com>
2022-10-20 12:16:30 +0300

    Classes

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace classes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Worker worker1 = new Worker();
+            worker1.Name="Yusuf Tolunay";
+            worker1.Surname="Çağlar";
+            worker1.PersonalID=11232201;
+            worker1.Department="Electrical & Elektronics Engineer";
+            worker1.WorkerInfo();
+
+            Worker worker2 = new Worker(){Name = "Berk", Surname="Koşucu", PersonalID=11232202, Department="Electrical & Elektronics Engineer"};
+            worker2.WorkerInfo();
+        }
+    }
+
+    class Worker
+    {
+        public string Name;
+        public string Surname;
+        public int PersonalID;
+        public string Department;
+
+        public void WorkerInfo(){
+            Console.WriteLine("Personal Information:");
+            Console.WriteLine("Name: {0}",this.Name);
+            Console.WriteLine("Surname: {0}",this.Surname);
+            Console.WriteLine("Personal ID no: {0}",this.PersonalID);
+            Console.WriteLine("Department: {0}",this.Department);
+        }
+    }
+}
